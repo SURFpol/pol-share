cce9ecf8269d2e31bd47b4564b1772380538c848
Fako Berkers <email@fakoberkers.nl>
2018-09-14 16:30:45 +0200

    Renames lti to service in order to be able to do more than just an LTI

---
+++ service/service/secrets.py
@@ -0,0 +1,1 @@
+SECRET_KEY = '_x$=&+fdbeslun@q-)+6cie&qif0w91-1h(y=ih5l9ay_e*(s9'
--- service/service/wsgi.py
+++ service/service/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for lti project.
+WSGI config for pol-share project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lti.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "service.settings")
 
 application = get_wsgi_application()
