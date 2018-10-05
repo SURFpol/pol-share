458c1760aba2ac44bb7760fa1cfc254eaef9b085
Fako Berkers <email@fakoberkers.nl>
2018-10-05 10:47:19 +0200

    Reads basic data from manifest

--- service/ims/models.py
+++ service/ims/models.py
@@ -1,6 +1,8 @@
 import os
 from zipfile import ZipFile
 
+from bs4 import BeautifulSoup
+
 from django.db import models
 from django import forms
 from django.core.exceptions import ValidationError
@@ -12,6 +14,34 @@ class CommonCartridge(models.Model):
     manifest = models.TextField(blank=True)
     upload_at = models.DateTimeField(auto_now_add=True)
 
+    def get_metadata(self):
+        manifest = BeautifulSoup(self.manifest, "lxml")
+        return {
+            'schema': {
+                'type': manifest.find('schema').text,
+                'version': manifest.find('schemaversion').text
+            },
+            'title': manifest.find('lomimscc:title').find('lomimscc:string').text,
+            'export_at': manifest.find('lomimscc:contribute').find('lomimscc:date').find('lomimscc:dateTime').text,
+            'license': manifest.find('lomimscc:rights').find('lomimscc:description').find('lomimscc:string').text
+        }
+
+    def get_content_tree(self):
+        manifest = BeautifulSoup(self.manifest, "lxml")
+        return manifest.find('organization').find('item')
+
+    def get_resources(self):
+        manifest = BeautifulSoup(self.manifest, "lxml")
+        results = {}
+        resources = manifest.find_all('resource', identifier=True)
+        for resource in resources:
+            results[resource['identifier']] = {
+                'content_type': resource['type'],
+                'main': resource['href'],
+                'files': [file['href'] for file in resource.find_all('file')]
+            }
+        return results
+
     def __str__(self):
         tail, head = os.path.split(self.file.name)
         return head
@@ -22,9 +52,10 @@ class CommonCartridgeForm(forms.ModelForm):
     def clean(self):
         cartridge = ZipFile(self.files['file'])
         try:
-            self.instance.manifest = str(cartridge.read('imsmanifest.xml'), encoding='utf-8')
+            self.instance.manifest = cartridge.read('imsmanifest.xml')
         except KeyError:
             raise ValidationError('The common cartridge should contain a manifest file')
+        # TODO: check metadata
         super().clean()
 
     class Meta:
