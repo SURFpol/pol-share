0d26d3decb617fab45fb458b851467a42df636fc
Fako Berkers <email@fakoberkers.nl>
2018-10-19 11:59:50 +0200

    Provides a way to logout

--- service/share/urls.py
+++ service/share/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from share.views import CommonCartridgeUploadView, CommonCartridgeDetailView
 
@@ -7,4 +8,5 @@ urlpatterns = [
     url(r'^upload/?$', CommonCartridgeUploadView.as_view(), name="common-cartridge-upload"),
     url(r'^upload/success/(?P<pk>[0-9]+)/?$', CommonCartridgeDetailView.as_view(),
         name="common-cartridge-upload-success"),
+    url(r'^exit/?$', auth_views.LogoutView.as_view(), name="share-exit"),
 ]
