459497a952cf060e374478a790e5872ab8ddd8ab
Fako Berkers <email@fakoberkers.nl>
2018-11-30 15:45:56 +0100

    Some window dressing for the flow

--- service/share/urls.py
+++ service/share/urls.py
@@ -6,7 +6,7 @@ from share.views import CommonCartridgeUploadView, CommonCartridgeDetailView, Co
 
 urlpatterns = [
     url(r'^fetch/?$', CommonCartridgeFetchViewset.login, name="common-cartridge-fetch"),
-    url(r'^fetch/start?$', CommonCartridgeFetchViewset.start, name="common-cartridge-fetch-start"),
+    url(r'^fetch/start/?$', CommonCartridgeFetchViewset.start, name="common-cartridge-fetch-start"),
     url(r'^upload/?$', CommonCartridgeUploadView.as_view(), name="common-cartridge-upload"),
     url(r'^upload/success/(?P<pk>[0-9]+)/?$', CommonCartridgeDetailView.as_view(),
         name="common-cartridge-upload-success"),
