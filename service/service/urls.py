12adf4fa98b1b8990381efaa97ab6c0c680eb45e
Fako Berkers <email@fakoberkers.nl>
2018-12-05 15:04:24 +0100

    Serve media files during debug

--- service/service/urls.py
+++ service/service/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views import static
 
 from ims import views as ims_views
 
@@ -17,6 +18,10 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns.append(url(r'^debug/(?P<slug>[A-Za-z0-9\-]+)/?$', ims_views.lti_debug_launch))
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', static.serve,
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True }),
+        url(r'^debug/(?P<slug>[A-Za-z0-9\-]+)/?$', ims_views.lti_debug_launch)
+    ]
 
 urlpatterns.append(url(r'^(?P<slug>[A-Za-z0-9\-]+)/?$', ims_views.lti_launch))
