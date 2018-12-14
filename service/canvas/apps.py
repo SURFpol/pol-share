ebf79ba71b1078ee87d2d548e341a2d82be78d2e
Fako Berkers <email@fakoberkers.nl>
2018-12-14 15:03:35 +0100

    Swaps configuration for a packaged version

--- service/canvas/apps.py
+++ service/canvas/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
+from dgconfig.types import register_config_defaults
+
+from canvas.constants import CanvasVisibility
 
 
 class CanvasConfig(AppConfig):
     name = 'canvas'
+
+    def ready(self):
+        register_config_defaults('ims', {
+            'canvas_course_navigation_visibility': CanvasVisibility.ADMINS
+        })
